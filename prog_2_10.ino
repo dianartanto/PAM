ca4541700b1686e4e24448759f5b5b02c8ab23a9
dianartanto <dian.artanto@gmail.com>
2025-06-21 07:17:11 +0700

    Add files via upload
    Program 2.10 Alarm dan notifikasi

---
+++ prog_2_10.ino
@@ -0,0 +1,63 @@
+#include <DHT.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <HX711.h>
+HX711 scale;
+Adafruit_SSD1306 oled(128, 64, &Wire, -1);
+DHT dht(33, DHT22);
+unsigned long skr = 0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(19, OUTPUT);
+  pinMode(18, OUTPUT);
+  pinMode(27, OUTPUT);
+  pinMode(14, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  scale.begin(26, 25);  //DT,SCK
+  if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    while (1)
+      ;
+  }
+  oled.setTextSize(2);
+  oled.setTextColor(WHITE);
+  oled.display();
+}
+void loop() {
+  if (millis() - skr > 100) {
+    float suhu = dht.readTemperature();
+    float kelm = dht.readHumidity();
+    uint16_t berat = round(scale.get_units() * 50 / 21);
+    Serial.println(berat);
+    oled.clearDisplay();
+    oled.setCursor(0, 0);
+    oled.println("Kelm:" + String(kelm, 1) + "%");
+    oled.setCursor(0, 24);
+    oled.println("Suhu:" + String(suhu, 1) + "C");
+    oled.setCursor(0, 48);
+    oled.println("Bert:" + String(berat) + "g");
+    oled.display();
+    if (suhu > 37) {
+      digitalWrite(19, LOW);
+      digitalWrite(18, HIGH);
+      digitalWrite(27, LOW);
+      digitalWrite(14, HIGH);
+      digitalWrite(12, LOW);
+    } else if (suhu < 35) {
+      digitalWrite(19, HIGH);
+      digitalWrite(18, LOW);
+      digitalWrite(27, HIGH);
+      digitalWrite(14, LOW);
+      digitalWrite(12, LOW);
+    } else {
+      digitalWrite(19, LOW);
+      digitalWrite(18, LOW);
+      digitalWrite(27, LOW);
+      digitalWrite(14, LOW);
+      digitalWrite(12, HIGH);
+    }
+    (kelm > 50) noTone(13);
+    else tone(13, 500);
+    skr = millis();
+  }
+}
